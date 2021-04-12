165105eede6a30726313a25771f3f66e6603c13b
Aaron <aaronfink02@gmail.com>
2021-04-12 12:13:36 -0700

    edited files for readme and fixed wsl vscode issue

--- webpack.config.js
+++ webpack.config.js
@@ -31,11 +31,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader',],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
-  mode: 'development',
+  mode: 'production',
   resolve: {
     extensions: ['.js', '.jsx'],
   },
